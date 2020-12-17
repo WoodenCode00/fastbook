987b7e3ea462cf05e7e4764a87ade870229ae475
mykhaylo-s <misha.arp@gmail.com>
2020-12-16 22:34:19 -0500

    Added project skeleton from https://start.spring.io (Spring Initializr)
    Java 8.
    Added maven wrapper.
    Added dependencies: SpringBoot Web, embedded H2 DB, Lombok, Devtools, Actuator, Thymeleaf template engine

---
+++ src/main/java/com/acme/fastbook/FastbookApplication.java
@@ -0,0 +1,13 @@
+package com.acme.fastbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastbookApplication.class, args);
+	}
+
+}
