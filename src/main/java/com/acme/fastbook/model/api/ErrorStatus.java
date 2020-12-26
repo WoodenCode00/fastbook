2bddcfba2c6afc922d268c0eb0473b1004f80c71
mykhaylo-s <misha.arp@gmail.com>
2020-12-26 15:01:40 -0500

    Exceptions: Added global exception handlers

---
+++ src/main/java/com/acme/fastbook/model/api/ErrorStatus.java
@@ -0,0 +1,14 @@
+package com.acme.fastbook.model.api;
+
+/**
+ * Enum with error statuses
+ * 
+ * @author Mykjhaylo Symulyk
+ *
+ */
+public enum ErrorStatus {
+	
+	RESOURCE_NOT_FOUND,
+	BAD_REQUEST,
+	INTERNAL_SERVER_ERROR
+}
--- src/main/java/com/acme/fastbook/model/helper/DateRangeHelper.java
+++ src/main/java/com/acme/fastbook/model/helper/DateRangeHelper.java
@@ -4,7 +4,6 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Optional;
 
 import com.acme.fastbook.model.DateRange;
 
